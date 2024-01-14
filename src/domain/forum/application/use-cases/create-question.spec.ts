ebc954bc71eddd14bd242420a20ca2ccbf17317a
Yuji Arima <arimayuji2@gmail.com>
2024-01-14 13:05:20 -0300

    feat: :sparkles: create a question
    its possible to create a question to the application forum

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'vitest'
+import { CreateQuestionUseCase } from './create-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CreateQuestionUseCase
+
+describe('Create Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to create a question', async () => {
+    const { question } = await sut.execute({
+      authorId: '1',
+      content: 'Hello World',
+      title: 'My first text',
+    })
+
+    expect(question.content).toEqual('Hello World')
+    expect(inMemoryQuestionsRepository.items[0].id).toEqual(question.id)
+  })
+})
