2b1ffcbb1b51ae8c954ba4ea9a4e6ff5dac9ea2c
Yuji Arima <arimayuji2@gmail.com>
2024-01-14 13:08:48 -0300

    feat: :sparkles: edit an answer
    its possible to edit a answer and only the author of it can do this action

---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,51 @@
+import { expect } from "vitest";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { makeAnswer } from "test/factories/make-answer";
+import { EditAnswerUseCase } from "./edit-answer";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: EditAnswerUseCase;
+
+describe("Edit Answer", () => {
+	beforeEach(() => {
+		inMemoryAnswersRepository = new InMemoryAnswersRepository();
+		sut = new EditAnswerUseCase(inMemoryAnswersRepository);
+	});
+
+	it("should be able to edit a answer", async () => {
+		const newAnswer = makeAnswer(
+			{ authorId: new UniqueEntityId("author-1") },
+			new UniqueEntityId("answer-1")
+		);
+
+		await inMemoryAnswersRepository.create(newAnswer);
+
+		await sut.execute({
+			authorId: "author-1",
+			answerId: newAnswer.id.toString(),
+			content: "Conteúdo Teste",
+		});
+
+		expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+			content: "Conteúdo Teste",
+		});
+	});
+
+	it("should not be able to edit a answer from another user", async () => {
+		const newAnswer = makeAnswer(
+			{ authorId: new UniqueEntityId("author-1") },
+			new UniqueEntityId("answer-1")
+		);
+
+		await inMemoryAnswersRepository.create(newAnswer);
+
+		await expect(() => {
+			return sut.execute({
+				authorId: "author-2",
+				answerId: newAnswer.id.toString(),
+				content: "Conteúdo Teste",
+			});
+		}).rejects.toBeInstanceOf(Error);
+	});
+});
