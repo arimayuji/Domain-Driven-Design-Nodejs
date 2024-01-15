ebae8288bf7c3a1c0b8cf0ac9cef930c0d572469
Yuji Arima <arimayuji2@gmail.com>
2024-01-15 12:33:39 -0300

    test: :test_tube: comment on answer sut
    tests if comment answer func works + tests if the only the  comment author can do it

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from "vitest";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { makeAnswer } from "test/factories/make-answer";
+import { CommentOnAnswerUseCase } from "./comment-on-answer";
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments";
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: CommentOnAnswerUseCase;
+
+describe("Comment on Answer", () => {
+	beforeEach(() => {
+		inMemoryAnswersRepository = new InMemoryAnswersRepository();
+		inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+
+		sut = new CommentOnAnswerUseCase(
+			inMemoryAnswersRepository,
+			inMemoryAnswerCommentsRepository
+		);
+	});
+
+	it("should be able to comment on  answer", async () => {
+		const answer = makeAnswer();
+
+		await inMemoryAnswersRepository.create(answer);
+
+		await sut.execute({
+			authorId: answer.authorId.toString(),
+			content: "Test Comment",
+			answerId: answer.id.toString(),
+		});
+
+		expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+			"Test Comment"
+		);
+	});
+});
