a02ca398ed8c3f586a86d0b3efe71d452f85f25e
Yuji Arima <arimayuji2@gmail.com>
2024-01-15 10:31:53 -0300

    test: :test_tube: fetch question asnwer sut
    tests fetch question answers case + tests answers paginated

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,47 @@
+import { expect } from "vitest";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { FetchQuestionsAnswersUseCase } from "./fetch-question-answers";
+import { makeAnswer } from "test/factories/make-answer";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: FetchQuestionsAnswersUseCase;
+
+describe("Fetch Answer Answers", () => {
+	beforeEach(() => {
+		inMemoryAnswersRepository = new InMemoryAnswersRepository();
+		sut = new FetchQuestionsAnswersUseCase(inMemoryAnswersRepository);
+	});
+
+	it("should be able to fetch question answers", async () => {
+		const questionId = new UniqueEntityId("question-1");
+
+		await inMemoryAnswersRepository.create(makeAnswer({ questionId }));
+
+		await inMemoryAnswersRepository.create(makeAnswer({ questionId }));
+
+		await inMemoryAnswersRepository.create(makeAnswer({ questionId }));
+
+		const { answers } = await sut.execute({
+			questionId: questionId.toString(),
+			page: 1,
+		});
+
+		expect(answers).toHaveLength(3);
+	});
+
+	it("should be able to fetch paginated question answers", async () => {
+		const questionId = new UniqueEntityId("question-1");
+
+		for (let i = 1; i <= 22; i++) {
+			await inMemoryAnswersRepository.create(makeAnswer({ questionId }));
+		}
+
+		const { answers } = await sut.execute({
+			questionId: questionId.toString(),
+			page: 2,
+		});
+
+		expect(answers).toHaveLength(2);
+	});
+});
