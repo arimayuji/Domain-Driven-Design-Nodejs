188d82da37481db063277c2fdc9f18f837bfcb06
Yuji Arima <arimayuji2@gmail.com>
2024-01-15 19:46:22 -0300

    feat: fetch question comments
    its possible to fetch all comments from a question based on id

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
 
@@ -26,5 +27,15 @@ export class InMemoryQuestionCommentsRepository
 		}
 
 		return questionComment;
+	}
+
+	async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+		const resultsPerPage = 20;
+
+		const questionComments = this.items
+			.filter((item) => item.questionId.toString() === questionId)
+			.slice((page - 1) * resultsPerPage, page * resultsPerPage);
+
+		return questionComments;
 	}
 }
