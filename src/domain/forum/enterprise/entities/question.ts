5d954c2bc5b83a1bcd7d5b22aa90ff182557087a
Yuji Arima <arimayuji2@gmail.com>
2024-01-15 10:40:18 -0300

    style: :art: question entity

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,79 +1,79 @@
-import { Slug } from './value-objects/slug'
-import { Entity } from '@/core/entities/entity'
-import { UniqueEntityId } from '@/core/entities/unique-entity-id'
-import { Optional } from '@/core/types/optional'
+import { Slug } from "./value-objects/slug";
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
 
 export interface QuestionProps {
-  title: string
-  content: string
-  slug: Slug
-  authorId: UniqueEntityId
-  createdAt: Date
-  updatedAt?: Date
-  bestAnswerId?: UniqueEntityId
+	title: string;
+	content: string;
+	slug: Slug;
+	authorId: UniqueEntityId;
+	createdAt: Date;
+	updatedAt?: Date;
+	bestAnswerId?: UniqueEntityId;
 }
 export class Question extends Entity<QuestionProps> {
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
+	private touch() {
+		this.props.updatedAt = new Date();
+	}
 
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
-  }
+	set content(content: string) {
+		this.props.content = content;
+		this.touch();
+	}
 
-  get content() {
-    return this.props.content
-  }
+	get content() {
+		return this.props.content;
+	}
 
-  set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
-    this.props.bestAnswerId = bestAnswerId
-    this.touch()
-  }
+	set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+		this.props.bestAnswerId = bestAnswerId;
+		this.touch();
+	}
 
-  get bestAnswerId() {
-    return this.props.bestAnswerId
-  }
+	get bestAnswerId() {
+		return this.props.bestAnswerId;
+	}
 
-  set title(title: string) {
-    this.props.title = title
-    this.props.slug = Slug.createFromText(title)
-    this.touch()
-  }
+	set title(title: string) {
+		this.props.title = title;
+		this.props.slug = Slug.createFromText(title);
+		this.touch();
+	}
 
-  get title() {
-    return this.props.title
-  }
+	get title() {
+		return this.props.title;
+	}
 
-  get authorId() {
-    return this.props.authorId
-  }
+	get authorId() {
+		return this.props.authorId;
+	}
 
-  get createdAt() {
-    return this.props.createdAt
-  }
+	get createdAt() {
+		return this.props.createdAt;
+	}
 
-  get slug() {
-    return this.props.slug
-  }
+	get slug() {
+		return this.props.slug;
+	}
 
-  get updatedAt() {
-    return this.props.updatedAt
-  }
+	get updatedAt() {
+		return this.props.updatedAt;
+	}
 
-  static create(
-    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
-    id?: UniqueEntityId,
-  ) {
-    const question = new Question(
-      {
-        ...props,
-        slug: props.slug ?? Slug.createFromText(props.title),
-        createdAt: new Date(),
-      },
-      id,
-    )
+	static create(
+		props: Optional<QuestionProps, "createdAt" | "slug">,
+		id?: UniqueEntityId
+	) {
+		const question = new Question(
+			{
+				...props,
+				slug: props.slug ?? Slug.createFromText(props.title),
+				createdAt: props.createdAt ?? new Date(),
+			},
+			id
+		);
 
-    return question
-  }
+		return question;
+	}
 }
