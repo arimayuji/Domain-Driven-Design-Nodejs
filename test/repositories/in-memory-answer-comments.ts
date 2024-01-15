1c4f5d06e645fe99b0495482421a64c45b9f7dfa
Yuji Arima <arimayuji2@gmail.com>
2024-01-15 12:28:31 -0300

    test: :test_tube: delete answer comment sut
    tests delete action works correctly + tests if thorw an error about different authors

---
+++ test/repositories/in-memory-answer-comments.ts
@@ -0,0 +1,28 @@
+import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
+import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+
+export class InMemoryAnswerCommentsRepository
+	implements AnswerCommentsRepository
+{
+	public items: AnswerComment[] = [];
+
+	async create(answerComment: AnswerComment) {
+		this.items.push(answerComment);
+	}
+	async delete(answerComment: AnswerComment) {
+		const itemIndex = this.items.findIndex(
+			(item) => item.id === answerComment.id
+		);
+
+		this.items.splice(itemIndex, 1);
+	}
+	async findById(id: string) {
+		const answerComment = this.items.find((item) => item.id.toString() === id);
+
+		if (!answerComment) {
+			return null;
+		}
+		
+		return answerComment;
+	}
+}
