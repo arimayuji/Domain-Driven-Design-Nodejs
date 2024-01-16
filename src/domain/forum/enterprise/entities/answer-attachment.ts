6909a5ea541766e9aabb7d43f89a7a2f5382a5c9
Yuji Arima <arimayuji2@gmail.com>
2024-01-16 18:21:08 -0300

    refactor: :recycle: attachment entity
    attachment entity is a  base class to the question-attachment and answer-attachment , it represents the relation between an attachment and question + answer entity

---
+++ src/domain/forum/enterprise/entities/answer-attachment.ts
@@ -0,0 +1,24 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+
+interface AnswerAttachmentProps {
+	questionId: UniqueEntityId;
+	attachmentId: UniqueEntityId;
+}
+
+export class AnswerAttachment extends Entity<AnswerAttachmentProps> {
+	get answerId() {
+		return this.props.questionId;
+	}
+
+	get attachmentId() {
+		return this.props.attachmentId;
+	}
+
+	static create(props:AnswerAttachmentProps, id?: UniqueEntityId) {
+		const attachment = new AnswerAttachment(props, id);
+
+		return attachment;
+	}
+}
